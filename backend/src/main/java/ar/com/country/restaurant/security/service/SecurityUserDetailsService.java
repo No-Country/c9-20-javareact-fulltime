c58f5783d7330ec1266e2572052f7aba81d2e1bf
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-03 22:32:50 -0600

    Fix SecurityUserDetailsService catching wrong exception

--- backend/src/main/java/ar/com/country/restaurant/security/service/SecurityUserDetailsService.java
+++ backend/src/main/java/ar/com/country/restaurant/security/service/SecurityUserDetailsService.java
@@ -1,13 +1,13 @@
 package ar.com.country.restaurant.security.service;
 
 import ar.com.country.restaurant.dao.entities.User;
+import ar.com.country.restaurant.exceptions.UserNotFoundException;
 import ar.com.country.restaurant.security.SecurityUser;
 import ar.com.country.restaurant.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,11 +16,11 @@ public class SecurityUserDetailsService implements UserDetailsService {
     private final UserService userService;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) {
         try {
             User user = userService.getUserByEmail(email);
             return new SecurityUser(user);
-        } catch (UsernameNotFoundException e) {
+        } catch (UserNotFoundException e) {
             throw new BadCredentialsException(e.getMessage());
         }
     }
