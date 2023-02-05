5c1962f09915cd14fc20b1f84f5dfd194dd79849
gaston964 <gastongutierrez964@gmail.com>
2023-02-05 16:19:14 -0300

    ITEM && ITEMLIST

---
+++ frontend/src/pages/home/components/ItemListContainer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+import Items from './components/Items'
+const Div = styled.div`
+display: flex;
+margin:15px;
+justify-content: space-around;
+flex-wrap: wrap;
+`
+
+const ItemListContainer = () => {
+    return (
+        <>
+        <Div>
+        <Items/>
+        <Items/>
+        <Items/>
+        <Items/>
+        <Items/>
+        </Div>
+        </>
+    )
+}
+
+export default ItemListContainer
--- frontend/src/pages/home/components/Items.jsx
+++ frontend/src/pages/home/components/Items.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-
+import React from 'react'
+import styled from 'styled-components'
+import { ItemButton, ItemContainer, ItemDescription, ItemImg } from '..styled-components/ItemsComponents'
 const Items = () => {
-	return <div>Items</div>;
-};
+return (
+    <>
+    <ItemContainer>
+        <h1>Pizza</h1>
+        <ItemImg>
+        <img src="\vite.svg" alt="image" />
+        </ItemImg>
+        <ItemDescription>
+        <h2>Descripcion</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi accusamus repellat impedit, iusto expedita distinctio, blanditiis corrupti libero veritatis quas iure vitae, atque cum exercitationem nam </p>
+        </ItemDescription>
+        <ItemButton>
+            <h4>Agregar al Carrito</h4>
+        </ItemButton>
+    </ItemContainer>
+    </>
+)
+}
 
-export default Items;
+export default Items
