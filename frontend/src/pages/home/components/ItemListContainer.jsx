4ae69b3379fe8fcecfc05c4440b713802a679e61
gaston964 <gastongutierrez964@gmail.com>
2023-02-05 16:42:42 -0300

    Detele Files

--- frontend/src/pages/home/components/ItemListContainer.jsx
+++ frontend/src/pages/home/components/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import Items from './components/Items'
+import Items from '../components/Items'
 const Div = styled.div`
 display: flex;
 margin:15px;
