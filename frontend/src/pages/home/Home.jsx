bd9c8a382e2ca0c27192c04ab2ec75fc38231556
marcio-git <marcio.dev.work@gmail.com>
2023-02-07 15:07:53 -0300

    Banner Component and Styles created + added in Home

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "./components/Banner";
 import Contact from "./components/Contact";
 import MyAccount from "./components/MyAccount";
 import Promotions from "./components/Promotions";
 const Home = () => {
 	return (
 		<>
+			<Banner />
 			<Promotions />
 			<Contact />
 			<MyAccount />
