82d82eaff71f2359b7221ab2df1b2d2df0110fd5
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-08 12:49:01 -0600

    Add base `/api` path to LoginAndRegistrationController

--- backend/src/main/java/ar/com/country/restaurant/web/controllers/LoginAndRegistrationController.java
+++ backend/src/main/java/ar/com/country/restaurant/web/controllers/LoginAndRegistrationController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@RestController
+@RestController("/api")
 @Validated
 public class LoginAndRegistrationController {
     private final UserService userService;
