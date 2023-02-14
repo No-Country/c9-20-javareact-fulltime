cdd0659b20c4873277eb3bf687115ad1f6650f49
Julio <alvarezjjoel6@gmail.com>
2023-02-14 02:10:05 -0300

    Add `UserControllerTest`

--- backend/src/main/java/ar/com/country/restaurant/repositories/UserRepository.java
+++ backend/src/main/java/ar/com/country/restaurant/repositories/UserRepository.java
@@ -1,7 +1,9 @@
 package ar.com.country.restaurant.repositories;
 
 import ar.com.country.restaurant.dao.entities.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,5 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
-
+    
 }
