4e84f0eb172a732ca233c8849b37fecfedc9c404
Julio <alvarezjjoel6@gmail.com>
2023-02-14 02:15:31 -0300

    Add methods in `UserService`

--- backend/src/main/java/ar/com/country/restaurant/services/UserService.java
+++ backend/src/main/java/ar/com/country/restaurant/services/UserService.java
@@ -3,9 +3,16 @@ package ar.com.country.restaurant.services;
 import ar.com.country.restaurant.dao.entities.User;
 
 public interface UserService {
-
-    User getUserByEmail(String username);
+    User findById(Long id);
 
     User createUser(User user);
+
+    User updateUser(User user);
+
+    void deleteUser(Long id);
+
+    boolean existsById(Long id);
+
+    User getUserByEmail(String username);
 
 }
