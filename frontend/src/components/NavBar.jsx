cb1852fa4ef8d1172e98f0c8fc7447fce5ede7e1
Daniel Felipe Amado <danielfep.am@gmail.com>
2023-02-14 12:01:01 -0500

    Fix - Hide NavBar Login and Signup routes

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
 import { NavBarStyled } from "../styled-components/NavBarComponent";
 
 const NavBar = () => {
+  const pathname = window.location.pathname
+  const routesNotShow = ["/login", "/signup"]
+  const showNavBar = !routesNotShow.includes(pathname)
+
   return (
-    <NavBarStyled>
-      LOGO
-      <input type="text" placeholder='Buscar'/>
-      <ul>
-        <li>Carta</li>
-        <li>Promociones</li>
-        <li>Contacto</li>
-        <li>Mi Cuenta</li>
-      </ul>
-      Cart
-    </NavBarStyled>
+    showNavBar && (
+      <NavBarStyled>
+        LOGO
+        <input type="text" placeholder='Buscar'/>
+        <ul>
+          <li>Carta</li>
+          <li>Promociones</li>
+          <li>Contacto</li>
+          <li>Mi Cuenta</li>
+        </ul>
+        Cart
+      </NavBarStyled>
+    )
   )
 }
 
