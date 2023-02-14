ca90b1f096f1acc66edfb4b522ee3470142375a4
Julio <alvarezjjoel6@gmail.com>
2023-02-14 13:01:32 -0300

    changing name

--- backend/src/test/java/ar/com/country/restaurant/web/controllers/UserControllerIT.java
+++ backend/src/test/java/ar/com/country/restaurant/web/controllers/UserControllerIT.java
@@ -24,7 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class UserControllerTest extends AbstractIntegrationTest {
+class UserControllerIT extends AbstractIntegrationTest {
 
     @Autowired
     private UserService userService;
