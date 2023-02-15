4cf89a95c0210a52d36e1d86a16d1cf6358e5e70
Julio <alvarezjjoel6@gmail.com>
2023-02-15 00:27:44 -0300

    Add `OpenAPI Bean` configuration

--- backend/src/main/java/ar/com/country/restaurant/CountryRestaurantApplication.java
+++ backend/src/main/java/ar/com/country/restaurant/CountryRestaurantApplication.java
@@ -1,7 +1,12 @@
 package ar.com.country.restaurant;
 
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CountryRestaurantApplication {
@@ -10,4 +15,15 @@ public class CountryRestaurantApplication {
         SpringApplication.run(CountryRestaurantApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription,
+                                 @Value("${application-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Country Restaurant API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("https://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
 }
