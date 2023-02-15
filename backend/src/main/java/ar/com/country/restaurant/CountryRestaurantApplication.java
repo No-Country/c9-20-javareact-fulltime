1b28716ae1a5cec5880e3729cba8a8f3c07edabf
Julio <alvarezjjoel6@gmail.com>
2023-02-15 14:37:22 -0300

    Add `SwaggerConfig`

--- backend/src/main/java/ar/com/country/restaurant/CountryRestaurantApplication.java
+++ backend/src/main/java/ar/com/country/restaurant/CountryRestaurantApplication.java
@@ -14,16 +14,4 @@ public class CountryRestaurantApplication {
     public static void main(String[] args) {
         SpringApplication.run(CountryRestaurantApplication.class, args);
     }
-
-    @Bean
-    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription,
-                                 @Value("${application-version}") String appVersion) {
-        return new OpenAPI()
-                .info(new io.swagger.v3.oas.models.info.Info()
-                        .title("Country Restaurant API")
-                        .version(appVersion)
-                        .description(appDescription)
-                        .termsOfService("https://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
-    }
 }
