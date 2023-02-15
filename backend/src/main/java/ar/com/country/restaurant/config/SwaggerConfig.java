2e57fea40c527b32d3c2a3136ba8090486f42e9d
Julio <alvarezjjoel6@gmail.com>
2023-02-15 14:37:58 -0300

    swagger config

--- backend/src/main/java/ar/com/country/restaurant/config/SwaggerConfig.java
+++ backend/src/main/java/ar/com/country/restaurant/config/SwaggerConfig.java
@@ -1,2 +1,23 @@
-package ar.com.country.restaurant.config;public class SwaggerConfig {
+package ar.com.country.restaurant.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription,
+                                 @Value("${application-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Country Restaurant API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("https://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
 }
