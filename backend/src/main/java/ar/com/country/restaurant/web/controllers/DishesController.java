d17e28434380881faee4e828927ad41b250c446d
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-15 11:46:20 -0600

    Add endpoint to search dishes by name and description

--- backend/src/main/java/ar/com/country/restaurant/web/controllers/DishesController.java
+++ backend/src/main/java/ar/com/country/restaurant/web/controllers/DishesController.java
@@ -4,6 +4,9 @@ import ar.com.country.restaurant.dao.entities.Dish;
 import ar.com.country.restaurant.services.DishesServices;
 import ar.com.country.restaurant.web.dto.DishDTO;
 import ar.com.country.restaurant.web.mappers.DishMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -20,18 +23,26 @@ public class DishesController {
     private final DishesServices dishesServices;
     private final DishMapper dishMapper;
 
-    public DishesController(DishesServices dishesServices, DishMapper dishMapper){
+    public DishesController(DishesServices dishesServices, DishMapper dishMapper) {
         this.dishesServices = dishesServices;
         this.dishMapper = dishMapper;
     }
 
+    @GetMapping("/search")
+    public Page<Dish> searchDishesByNameAndDescription(
+            @RequestParam String query,
+            @PageableDefault Pageable pageable
+    ) {
+        return dishesServices.searchDishByNameAndDescription(query, pageable);
+    }
+
     @GetMapping("/")
-    public List<Dish> findAllDishes(){
+    public List<Dish> findAllDishes() {
         return dishesServices.getAllDishes();
     }
 
     @GetMapping("/{id}")
-    public Optional<Dish> getDishById(@PathVariable Long id){
+    public Optional<Dish> getDishById(@PathVariable Long id) {
         return dishesServices.findById(id);
     }
 
@@ -43,14 +54,14 @@ public class DishesController {
     }
 
     @PutMapping("/{id}")
-    public Dish updateDish(@PathVariable Long id, @RequestBody Dish newDish){
+    public Dish updateDish(@PathVariable Long id, @RequestBody Dish newDish) {
         Optional<Dish> oldDish = dishesServices.findById(id);
         newDish.setId(oldDish.get().getId());
         return dishesServices.createDish(newDish);
     }
 
     @DeleteMapping("/{id}")
-    public Dish deleteDishById(@PathVariable Long id){
+    public Dish deleteDishById(@PathVariable Long id) {
         return dishesServices.deleteById(id);
     }
 
