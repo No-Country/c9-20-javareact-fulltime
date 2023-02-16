717cc2eb45a3572ae63f458f1d57459187f9340c
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-16 12:50:53 -0600

    Add documentation to user API

--- backend/src/main/java/ar/com/country/restaurant/web/controllers/UserController.java
+++ backend/src/main/java/ar/com/country/restaurant/web/controllers/UserController.java
@@ -5,25 +5,51 @@ import ar.com.country.restaurant.services.UserService;
 import ar.com.country.restaurant.web.dto.UserDTO;
 import ar.com.country.restaurant.web.hateoas.assemblers.UserModelAssembler;
 import ar.com.country.restaurant.web.mappers.UserMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+import static ar.com.country.restaurant.util.ApiDocsConstants.*;
+
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
+@Tag(name = "Users", description = "API to manage users")
+@ApiResponses({
+        @ApiResponse(ref = BAD_REQUEST_RESPONSE_REF, responseCode = "400"),
+        @ApiResponse(ref = UNAUTHORIZED_RESPONSE_REF, responseCode = "401"),
+        @ApiResponse(ref = NOT_FOUND_RESPONSE_REF, responseCode = "404"),
+        @ApiResponse(ref = INTERNAL_SERVER_ERROR_RESPONSE_REF, responseCode = "500")
+})
 public class UserController {
     private final UserService userService;
     private final UserMapper userMapper;
     private final UserModelAssembler userModelAssembler;
 
+    @Operation(summary = "Returns user associated with the given ID")
+    @ApiResponse(responseCode = "200", description = "OK", content = {
+            @Content(schema = @Schema(implementation = UserDTO.class))
+    })
     @GetMapping("/{userId}")
     public UserDTO getUserById(@PathVariable Long userId) {
         User result = userService.getUserById(userId);
         return userModelAssembler.toModel(result);
     }
 
+    @Operation(summary = "Updates user with the given payload")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User updated", content = {
+                    @Content(schema = @Schema(implementation = UserDTO.class))
+            }),
+            @ApiResponse(ref = FORBIDDEN_RESPONSE_REF, responseCode = "403")
+    })
     @PutMapping("/{userId}")
     public UserDTO updateUser(@PathVariable Long userId, @RequestBody @Valid UserDTO userDto) {
         User updatedUser = userMapper.toEntity(userDto);
@@ -31,6 +57,13 @@ public class UserController {
         return userModelAssembler.toModel(result);
     }
 
+    @Operation(summary = "Deletes user with the given payload")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "User deleted", content = {
+                    @Content(schema = @Schema(implementation = UserDTO.class))
+            }),
+            @ApiResponse(ref = FORBIDDEN_RESPONSE_REF, responseCode = "403")
+    })
     @DeleteMapping("/{userId}")
     public UserDTO deleteUser(@PathVariable Long userId) {
         User result = userService.deleteUser(userId);
