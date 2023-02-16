c3f81f703570fce56f9599473b6d4fc31ea7e3ae
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-15 21:29:14 -0600

    Add simple HATEOAS support for payment method controller

--- backend/src/main/java/ar/com/country/restaurant/web/dto/PaymentMethodDTO.java
+++ backend/src/main/java/ar/com/country/restaurant/web/dto/PaymentMethodDTO.java
@@ -3,14 +3,18 @@ package ar.com.country.restaurant.web.dto;
 import ar.com.country.restaurant.dao.entities.CardType;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
-public final class PaymentMethodDTO implements Serializable {
+@EqualsAndHashCode(callSuper = true)
+@Relation(itemRelation = "payment_method", collectionRelation = "payment_methods")
+public final class PaymentMethodDTO extends RepresentationModel<PaymentMethodDTO> {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private final Long id;
 
