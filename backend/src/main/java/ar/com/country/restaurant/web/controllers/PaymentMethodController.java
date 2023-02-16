77158f54de124d5cc25cbe3689355306eb99e2a2
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-16 16:39:31 -0600

    Add documentation to payment method API

--- backend/src/main/java/ar/com/country/restaurant/web/controllers/PaymentMethodController.java
+++ backend/src/main/java/ar/com/country/restaurant/web/controllers/PaymentMethodController.java
@@ -6,6 +6,13 @@ import ar.com.country.restaurant.services.PaymentMethodService;
 import ar.com.country.restaurant.web.dto.PaymentMethodDTO;
 import ar.com.country.restaurant.web.hateoas.assemblers.PaymentMethodModelAssembler;
 import ar.com.country.restaurant.web.mappers.PaymentMethodMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +23,26 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+import static ar.com.country.restaurant.util.ApiDocsConstants.*;
+
 @RestController
 @RequestMapping("/api/payment_methods")
 @RequiredArgsConstructor
+@Tag(name = "Payment methods", description = "API to manage payment methods")
+@ApiResponses({
+        @ApiResponse(ref = BAD_REQUEST_RESPONSE_REF, responseCode = "400"),
+        @ApiResponse(ref = UNAUTHORIZED_RESPONSE_REF, responseCode = "401"),
+        @ApiResponse(ref = INTERNAL_SERVER_ERROR_RESPONSE_REF, responseCode = "500")
+})
 public class PaymentMethodController {
     private final PaymentMethodService paymentMethodService;
     private final PaymentMethodMapper paymentMethodMapper;
     private final PaymentMethodModelAssembler paymentMethodModelAssembler;
 
+    @Operation(summary = "Returns the payment methods of the logged user")
+    @ApiResponse(responseCode = "200", description = "OK", content = {
+            @Content(array = @ArraySchema(schema = @Schema(implementation = PaymentMethodDTO.class)))
+    })
     @GetMapping
     public CollectionModel<PaymentMethodDTO> getPaymentMethodsOfLoggedUser(
             @AuthenticationPrincipal SecurityUser loggedUser
@@ -33,6 +52,13 @@ public class PaymentMethodController {
         return paymentMethodModelAssembler.toCollectionModel(result);
     }
 
+    @Operation(summary = "Returns the payment method associated with the given ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Payment method found", content = {
+                    @Content(schema = @Schema(implementation = PaymentMethodDTO.class))
+            }),
+            @ApiResponse(ref = NOT_FOUND_RESPONSE_REF, responseCode = "404")
+    })
     @GetMapping("/{paymentMethodId}")
     public PaymentMethodDTO getPaymentMethodByIdOfLoggedUser(
             @AuthenticationPrincipal SecurityUser loggedUser,
@@ -43,6 +69,10 @@ public class PaymentMethodController {
         return paymentMethodModelAssembler.toModel(result);
     }
 
+    @Operation(summary = "Adds a payment method to the logged user")
+    @ApiResponse(responseCode = "201", description = "Payment method added", content = {
+            @Content(schema = @Schema(implementation = PaymentMethodDTO.class))
+    })
     @PostMapping
     public ResponseEntity<PaymentMethodDTO> addPaymentMethodToLoggedUser(
             @AuthenticationPrincipal SecurityUser loggedUser,
@@ -57,6 +87,13 @@ public class PaymentMethodController {
 
     }
 
+    @Operation(summary = "Updates a payment method with the given payload. All fields must be provided.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Payment method updated", content = {
+                    @Content(schema = @Schema(implementation = PaymentMethodDTO.class))
+            }),
+            @ApiResponse(ref = NOT_FOUND_RESPONSE_REF, responseCode = "404")
+    })
     @PutMapping("/{paymentMethodId}")
     public PaymentMethodDTO updatePaymentMethodOfLoggedUser(
             @AuthenticationPrincipal SecurityUser loggedUser,
@@ -69,6 +106,13 @@ public class PaymentMethodController {
         return paymentMethodModelAssembler.toModel(result);
     }
 
+    @Operation(summary = "Deletes a payment method associated with the given ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Payment method deleted", content = {
+                    @Content(schema = @Schema(implementation = PaymentMethodDTO.class))
+            }),
+            @ApiResponse(ref = NOT_FOUND_RESPONSE_REF, responseCode = "404")
+    })
     @DeleteMapping("/{paymentMethodId}")
     public PaymentMethodDTO deletePaymentMethod(
             @AuthenticationPrincipal SecurityUser loggedUser,
