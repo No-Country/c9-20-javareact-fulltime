e8ae2827669c9bcaca6d4b333eb3a42a053c6e9b
Julio <alvarezjjoel6@gmail.com>
2023-02-16 00:38:13 -0300

    Add DishCategoryService implementation

--- backend/src/main/java/ar/com/country/restaurant/services/impl/DishCategoryServiceImpl.java
+++ backend/src/main/java/ar/com/country/restaurant/services/impl/DishCategoryServiceImpl.java
@@ -1,2 +1,45 @@
-package ar.com.country.restaurant.services.impl;public class DishCategoryServiceImpl {
+package ar.com.country.restaurant.services.impl;
+
+import ar.com.country.restaurant.dao.entities.DishCategory;
+import ar.com.country.restaurant.exceptions.DishCategoryNotFoundException;
+import ar.com.country.restaurant.repositories.DishCategoryRepository;
+import ar.com.country.restaurant.services.DishCategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DishCategoryServiceImpl implements DishCategoryService {
+
+    private DishCategoryRepository dishCategoryRepository;
+
+    @Override
+    public List<DishCategory> getAllDishCategories() {
+        return dishCategoryRepository.findAllDishCategoriesByOrderByNameAsc();
+    }
+
+    @Override
+    public DishCategory createDishCategory(DishCategory dishCategory) {
+        return dishCategoryRepository.save(dishCategory);
+    }
+
+    @Override
+    public DishCategory deleteDishCategory(Long id) {
+        dishCategoryRepository.deleteById(id);
+        return null;
+    }
+
+    @Override
+    public Optional<DishCategory> findById(Long id) {
+        existsDishCategory(id);
+        return dishCategoryRepository.findById(id);
+    }
+
+    private void existsDishCategory(Long id) {
+        dishCategoryRepository.findById(id).orElseThrow(() -> new DishCategoryNotFoundException());
+    }
+
 }
