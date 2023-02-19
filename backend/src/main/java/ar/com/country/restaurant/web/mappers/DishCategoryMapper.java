dee8a9640ac68098b6802872709e29bf04c7fe37
Julio <alvarezjjoel6@gmail.com>
2023-02-18 21:24:46 -0300

    Remove `toResponseDto` method in DishCategoryMapper

--- backend/src/main/java/ar/com/country/restaurant/web/mappers/DishCategoryMapper.java
+++ backend/src/main/java/ar/com/country/restaurant/web/mappers/DishCategoryMapper.java
@@ -1,12 +1,8 @@
 package ar.com.country.restaurant.web.mappers;
 
-import ar.com.country.restaurant.dao.entities.Dish;
 import ar.com.country.restaurant.dao.entities.DishCategory;
 import ar.com.country.restaurant.web.dto.DishCategoryDTO;
-import ar.com.country.restaurant.web.dto.DishCategoryResponseDTO;
-import ar.com.country.restaurant.web.dto.DishResponseDTO;
 import org.mapstruct.Mapper;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -18,6 +14,4 @@ public interface DishCategoryMapper {
     DishCategoryDTO toDto(DishCategory dishCategory);
 
     List<DishCategoryDTO> toDtoList(List<DishCategory> dishCategoryList);
-
-    DishCategoryResponseDTO toResponseDto(DishCategory dishCategory);
 }
