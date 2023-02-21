09f7713f5bddbc47d35f777bc44a9b3cb33ecada
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-02-20 21:42:55 -0600

    Fix `dishes` link in `DishCategoryModelAssembler` class

--- backend/src/main/java/ar/com/country/restaurant/web/hateoas/assemblers/DishCategoryModelAssembler.java
+++ backend/src/main/java/ar/com/country/restaurant/web/hateoas/assemblers/DishCategoryModelAssembler.java
@@ -2,32 +2,32 @@ package ar.com.country.restaurant.web.hateoas.assemblers;
 
 import ar.com.country.restaurant.dao.entities.DishCategory;
 import ar.com.country.restaurant.web.controllers.DishCategoryController;
+import ar.com.country.restaurant.web.controllers.DishController;
 import ar.com.country.restaurant.web.dto.DishCategoryDTO;
 import ar.com.country.restaurant.web.mappers.DishCategoryMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 @RequiredArgsConstructor
 public class DishCategoryModelAssembler implements RepresentationModelAssembler<DishCategory, DishCategoryDTO> {
-
     private final DishCategoryMapper dishCategoryMapper;
-
     private DishCategory dishCategory;
 
     @Override
-    public DishCategoryDTO toModel(DishCategory dishCategory) {
+    @NonNull
+    public DishCategoryDTO toModel(@NonNull DishCategory dishCategory) {
         this.dishCategory = dishCategory;
         DishCategoryDTO dishCategoryDTO = dishCategoryMapper.toDto(dishCategory);
-        dishCategoryDTO.add(selfLink());
-        dishCategoryDTO.add(dishesLink());
+        dishCategoryDTO.add(selfLink(), dishesLink());
         return dishCategoryDTO;
     }
-
 
     private Link selfLink() {
         return linkTo(DishCategoryController.class)
@@ -36,9 +36,9 @@ public class DishCategoryModelAssembler implements RepresentationModelAssembler<
     }
 
     private Link dishesLink() {
-        return linkTo(DishCategoryController.class)
-                .slash(dishCategory.getId())
-                .slash("dishes")
-                .withRel("dishes");
+        return linkTo(methodOn(DishController.class)
+                .getDishes(dishCategory.getId(), null)
+        ).withRel("dishes");
     }
+
 }
