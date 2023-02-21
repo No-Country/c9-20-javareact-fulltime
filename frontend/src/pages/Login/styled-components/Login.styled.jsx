c46b045536034230d6adbd2aa7e01984bc94b5b4
SeabastianDeLaCruz <sebastiandelacruz335@gmial.com>
2023-02-21 12:23:10 -0300

    cambios del 21/2/2023

---
+++ frontend/src/pages/Login/styled-components/Login.styled.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const ForgotPassword = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items:center;
+  & > a {
+    text-decoration: none;
+    color: #4D4D4D;
+  }
+`;
