51da789932409c6ce45ebf6bdb31ce28decacc6c
Julio <alvarezjjoel6@gmail.com>
2023-02-22 01:51:57 -0300

    Add `CartRepository`

--- backend/src/main/java/ar/com/country/restaurant/repositories/CartRepository.java
+++ backend/src/main/java/ar/com/country/restaurant/repositories/CartRepository.java
@@ -1,2 +1,17 @@
-package ar.com.country.restaurant.repositories;public interface CartRepository {
+package ar.com.country.restaurant.repositories;
+
+import ar.com.country.restaurant.dao.entities.Cart;
+import ar.com.country.restaurant.dao.entities.ItemCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    @Query("select i from ItemCart i where i.cart.id = ?1")
+    List<ItemCart> findAllItemsFromCart(Long cartId);
+
 }
