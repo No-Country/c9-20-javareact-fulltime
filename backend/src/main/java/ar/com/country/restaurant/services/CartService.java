e5dfcf1e9c56542177298049c3e6c41afa2c74ec
Julio <alvarezjjoel6@gmail.com>
2023-02-22 01:52:22 -0300

    Add `CartService`

--- backend/src/main/java/ar/com/country/restaurant/services/CartService.java
+++ backend/src/main/java/ar/com/country/restaurant/services/CartService.java
@@ -1,2 +1,16 @@
-package ar.com.country.restaurant.services;public interface CartService {
+package ar.com.country.restaurant.services;
+
+import ar.com.country.restaurant.dao.entities.Cart;
+import ar.com.country.restaurant.dao.entities.Dish;
+import ar.com.country.restaurant.dao.entities.ItemCart;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CartService {
+
+    List<ItemCart> getItemsFromCart(Long cartId);
+
+    Cart saveCart(Cart cart);
+
 }
