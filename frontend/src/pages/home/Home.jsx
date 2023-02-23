52024f58ed7c991193074a74d3df5754ea099dc2
Daniel Felipe Amado <danielfep.am@gmail.com>
2023-02-23 17:31:49 -0500

    Add - Private routes

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,12 @@
 import { CardList, Footer, Header } from "../../components";
 import CustomerFeedback from "./components/CustomerFeedback";
 import SectionHero from "./components/SectionHero";
+import useAuth from '../../hooks/useAuth'
 
 const Home = () => {
+  
+  useAuth()
+
   return (
     <>
       <Header />
