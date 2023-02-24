ff63297692c01e2f8469f61022cde82b8cf6a99b
Julio <alvarezjjoel6@gmail.com>
2023-02-24 00:22:46 -0300

    Add new methods in `CartRepository`

--- backend/src/main/java/ar/com/country/restaurant/repositories/CartRepository.java
+++ backend/src/main/java/ar/com/country/restaurant/repositories/CartRepository.java
@@ -7,11 +7,22 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
+    @Query("select c from Cart c where c.user.id = ?1")
+    Optional<Cart> findCartByUserId(Long userId);
+
+    @Query("select c from Cart c where c.id = ?1 and c.user.id = ?2")
+    Optional<Cart> findCartByIdAndUserId(Long cartId, Long userId);
+
     @Query("select i from ItemCart i where i.cart.id = ?1")
     List<ItemCart> findAllItemsFromCart(Long cartId);
+
+
+
+
 
 }
