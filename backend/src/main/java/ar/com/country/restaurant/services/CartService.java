30181219b18d637f1ba118fbb5d2ed65f8710d5a
Julio <alvarezjjoel6@gmail.com>
2023-02-24 00:23:14 -0300

    Add new methods in `CartService`

--- backend/src/main/java/ar/com/country/restaurant/services/CartService.java
+++ backend/src/main/java/ar/com/country/restaurant/services/CartService.java
@@ -1,16 +1,20 @@
 package ar.com.country.restaurant.services;
 
 import ar.com.country.restaurant.dao.entities.Cart;
-import ar.com.country.restaurant.dao.entities.Dish;
 import ar.com.country.restaurant.dao.entities.ItemCart;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface CartService {
 
+    Cart getCartOfLoggedUser(Long userId);
+
+    Cart getCartById(Long userId, Long cartId);
+
     List<ItemCart> getItemsFromCart(Long cartId);
 
-    Cart saveCart(Cart cart);
+    Cart addItem(Long userId, ItemCart itemCart);
+
+    Cart deleteItem(Long userId, Long itemId);
 
 }
