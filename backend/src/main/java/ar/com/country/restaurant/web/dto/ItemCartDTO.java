c44f92770bd1b3bc36ad2811a90e19112712ef72
Julio <alvarezjjoel6@gmail.com>
2023-02-24 00:25:02 -0300

    Add `ItemCartDTO`

--- backend/src/main/java/ar/com/country/restaurant/web/dto/ItemCartDTO.java
+++ backend/src/main/java/ar/com/country/restaurant/web/dto/ItemCartDTO.java
@@ -1,2 +1,25 @@
-package ar.com.country.restaurant.web.dto;public class ItemCartDTO {
+package ar.com.country.restaurant.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@EqualsAndHashCode
+public class ItemCartDTO {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long cartId;
+
+    @NotNull
+    private Long dishId;
+
+    @NotNull
+    private int quantity;
+
 }
