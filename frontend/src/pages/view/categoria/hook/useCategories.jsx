c43ee5b4cc53c39e94ed9c27764c6ea71df5b3c5
Gaston Gutierrez <gastongutierrez964@gmail.com>
2023-02-24 00:24:21 -0300

    Fix Routes

--- frontend/src/pages/view/categoria/hook/useCategories.jsx
+++ frontend/src/pages/view/categoria/hook/useCategories.jsx
@@ -1,21 +1,22 @@
 import { useEffect, useState } from "react";
 import { useGetCategoriesQuery } from "../../../../redux/query/FoodInfo.query";
 const useCategories = () => {
-	const [categories, setCategories] = useState([]);
+    const [categories, setCategories] = useState([]);
 
-	const { data: cate, isLoading } = useGetCategoriesQuery();
+    const { data: cate, isLoading } = useGetCategoriesQuery();
 
-	useEffect(() => {
-		if (!isLoading) {
-			const { dishCategories } = cate._embedded;
-			setCategories([...dishCategories]);
-		}
-	}, [cate]);
-	console.log(categories);
-	return {
-		categories,
-		isLoading,
-	};
+    useEffect(() => {
+		console.log(cate);
+        if (!isLoading) {
+            const { dishCategories } = cate._embedded
+            setCategories([...dishCategories]);
+        }
+    }, [cate]);
+
+    return {
+        categories,
+        isLoading,
+    };
 };
 
 export default useCategories;
