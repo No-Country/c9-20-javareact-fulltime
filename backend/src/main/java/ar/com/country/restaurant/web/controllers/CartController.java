097f9e6ca32519a3b01664fec2b9fa94122549ba
Julio <alvarezjjoel6@gmail.com>
2023-02-24 00:21:47 -0300

    Refactor `CartController`

--- backend/src/main/java/ar/com/country/restaurant/web/controllers/CartController.java
+++ backend/src/main/java/ar/com/country/restaurant/web/controllers/CartController.java
@@ -1,82 +1,58 @@
 package ar.com.country.restaurant.web.controllers;
 
 import ar.com.country.restaurant.dao.entities.Cart;
-import ar.com.country.restaurant.dao.entities.Dish;
 import ar.com.country.restaurant.dao.entities.ItemCart;
-import ar.com.country.restaurant.dao.entities.User;
+import ar.com.country.restaurant.security.SecurityUser;
 import ar.com.country.restaurant.services.CartService;
-import ar.com.country.restaurant.services.DishService;
-import ar.com.country.restaurant.services.UserService;
+import ar.com.country.restaurant.web.dto.ItemCartDTO;
+import ar.com.country.restaurant.web.mappers.ItemCartMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/carts")
+@RequiredArgsConstructor
 public class CartController {
 
     private final CartService cartService;
 
-    private final DishService dishService;
+    private final ItemCartMapper itemCartMapper;
 
-    private final UserService userService;
 
-    public CartController(CartService cartService, DishService dishService, UserService userService) {
-        this.cartService = cartService;
-        this.dishService = dishService;
-        this.userService = userService;
+    @GetMapping
+    public Cart getCartOfLoggedUser(
+            @AuthenticationPrincipal SecurityUser securityUser
+    ) {
+        Long userId = securityUser.getId();
+        return cartService.getCartOfLoggedUser(userId);
     }
 
-    @GetMapping("/{userId}")
-    public Cart getCartByUserId(@PathVariable Long userId) {
-        User user = userService.getUserById(userId);
-        return user.getCart();
+
+    @GetMapping("/{cartId}")
+    public Cart getCartById(
+            @AuthenticationPrincipal SecurityUser securityUser,
+            @PathVariable Long cartId
+    ) {
+        Long userId = securityUser.getId();
+        return cartService.getCartById(userId, cartId);
     }
+
 
     @GetMapping("/{userId}/items")
     public List<ItemCart> getItemsFromCart(@PathVariable Long userId) {
-        User user = userService.getUserById(userId);
-        Cart cart = user.getCart();
-        return cartService.getItemsFromCart(cart.getId());
+        return cartService.getItemsFromCart(userId);
     }
 
     @PostMapping("/{userId}/items")
-    public Cart addItemToCart(@PathVariable Long userId, @RequestBody ItemCart itemCart) {
-
-        User user = userService.getUserById(userId);
-        Dish dish = dishService.getDishById(itemCart.getDish().getId());
-        Cart cart = user.getCart();
-
-        ItemCart item = new ItemCart();
-        item.setDish(dish);
-        item.setQuantity(itemCart.getQuantity());
-
-        cart = createOrUpdateCart(user, cart);
-        cart.addItemCart(item);
-        cart.calculateSubTotal();
-
-        cartService.saveCart(cart);
-        return cart;
+    public Cart addItemToCart(@PathVariable Long userId, @RequestBody ItemCartDTO itemCart) {
+        return cartService.addItem(userId, itemCartMapper.toEntity(itemCart));
     }
 
     @DeleteMapping("/{userId}/items/{itemId}")
     public Cart removeItemFromCart(@PathVariable Long userId, @PathVariable Long itemId) {
-        User user = userService.getUserById(userId);
-        Cart cart = user.getCart();
-        ItemCart item = cart.getItems().stream().filter(i -> i.getId().equals(itemId)).findFirst().get();
-        cart.removeItemCart(item);
-        cart.calculateSubTotal();
-        cartService.saveCart(cart);
-        return cart;
+        return cartService.deleteItem(userId, itemId);
     }
-
-    private static Cart createOrUpdateCart(User user, Cart cart) {
-        if (cart == null) {
-            cart = new Cart();
-            cart.setUser(user);
-            user.setCart(cart);
-        }
-        return cart;
-    }
-
 }
