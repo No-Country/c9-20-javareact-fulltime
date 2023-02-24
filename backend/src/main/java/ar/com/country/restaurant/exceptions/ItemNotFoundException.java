7bf448a5a15e7ecbe717fd2498e092a94d6fcb80
Julio <alvarezjjoel6@gmail.com>
2023-02-24 00:26:03 -0300

    Add `ItemNotFoundException`

--- backend/src/main/java/ar/com/country/restaurant/exceptions/ItemNotFoundException.java
+++ backend/src/main/java/ar/com/country/restaurant/exceptions/ItemNotFoundException.java
@@ -1,2 +1,8 @@
-package ar.com.country.restaurant.exceptions;public class ItemNotFoundException {
+package ar.com.country.restaurant.exceptions;
+
+public class ItemNotFoundException extends RuntimeException {
+
+        public ItemNotFoundException(String message) {
+            super(message);
+        }
 }
