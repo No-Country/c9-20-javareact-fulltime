32ca4b429bf0a663a02d0441b39b78c8390e4d3a
Manuel <nacermanuel@gmail.com>
2023-02-26 10:02:49 -0500

    cambios en el checkout

--- frontend/src/redux/slice/cart.slice.js
+++ frontend/src/redux/slice/cart.slice.js
@@ -2,50 +2,52 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
 	open: 0,
 	// Por prueba se inicia el state con 2 items, BORRAR luego de prueba.
-	items: [{
-		"id": 1,
-		"name": "Combo libra",
-		"description": "Hamburguesa, lechuga, tomate, cebolla morada, pepino, y cheddar + papas",
-		"price": 3500.0,
-		"image": {
-			"url": "https://res.cloudinary.com/doxahduh8/image/upload/v1676594926/Dise%C3%B1o_sin_t%C3%ADtulo_-_2023-02-15T183907_1_cbjj8f.png"
-		},
-		"category": {
-			"id": 6,
-			"imgUrl": "https://res.cloudinary.com/doxahduh8/image/upload/v1676594931/Dise%C3%B1o_sin_t%C3%ADtulo_88_1_btb96u.png",
-			"name": "Churrasqueria"
-		},
-		"_links": {
-			"self": {
-				"href": "http://localhost:8080/api/dishes/1"
-			},
-			"category": {
-				"href": "http://localhost:8080/api/categories/6"
-			}
-		}
-	},
-	{
-		"id": 2,
-		"name": "Tabla I",
-		"description": "Carne salteada, aceituna, salame, queso roquefort, queso caprese, salsa picante",
-		"price": 2500.0,
-		"image": {
-			"url": "https://res.cloudinary.com/doxahduh8/image/upload/v1676594925/Dise%C3%B1o_sin_t%C3%ADtulo_90_1_gatcrg.png"
-		},
-		"category": {
-			"id": 6,
-			"imgUrl": "https://res.cloudinary.com/doxahduh8/image/upload/v1676594931/Dise%C3%B1o_sin_t%C3%ADtulo_88_1_btb96u.png",
-			"name": "Churrasqueria"
-		},
-		"_links": {
-			"self": {
-				"href": "http://localhost:8080/api/dishes/2"
-			},
-			"category": {
-				"href": "http://localhost:8080/api/categories/6"
-			}
-		}
-	}],
+	items: [
+	// 	{
+	// 	"id": 1,
+	// 	"name": "Combo libra",
+	// 	"description": "Hamburguesa, lechuga, tomate, cebolla morada, pepino, y cheddar + papas",
+	// 	"price": 3500.0,
+	// 	"image": {
+	// 		"url": "https://res.cloudinary.com/doxahduh8/image/upload/v1676594926/Dise%C3%B1o_sin_t%C3%ADtulo_-_2023-02-15T183907_1_cbjj8f.png"
+	// 	},
+	// 	"category": {
+	// 		"id": 6,
+	// 		"imgUrl": "https://res.cloudinary.com/doxahduh8/image/upload/v1676594931/Dise%C3%B1o_sin_t%C3%ADtulo_88_1_btb96u.png",
+	// 		"name": "Churrasqueria"
+	// 	},
+	// 	"_links": {
+	// 		"self": {
+	// 			"href": "http://localhost:8080/api/dishes/1"
+	// 		},
+	// 		"category": {
+	// 			"href": "http://localhost:8080/api/categories/6"
+	// 		}
+	// 	}
+	// },
+	// {
+	// 	"id": 2,
+	// 	"name": "Tabla I",
+	// 	"description": "Carne salteada, aceituna, salame, queso roquefort, queso caprese, salsa picante",
+	// 	"price": 2500.0,
+	// 	"image": {
+	// 		"url": "https://res.cloudinary.com/doxahduh8/image/upload/v1676594925/Dise%C3%B1o_sin_t%C3%ADtulo_90_1_gatcrg.png"
+	// 	},
+	// 	"category": {
+	// 		"id": 6,
+	// 		"imgUrl": "https://res.cloudinary.com/doxahduh8/image/upload/v1676594931/Dise%C3%B1o_sin_t%C3%ADtulo_88_1_btb96u.png",
+	// 		"name": "Churrasqueria"
+	// 	},
+	// 	"_links": {
+	// 		"self": {
+	// 			"href": "http://localhost:8080/api/dishes/2"
+	// 		},
+	// 		"category": {
+	// 			"href": "http://localhost:8080/api/categories/6"
+	// 		}
+	// 	}
+	// }
+],
 	total: 0,
 };
 export const CartSlice = createSlice({
