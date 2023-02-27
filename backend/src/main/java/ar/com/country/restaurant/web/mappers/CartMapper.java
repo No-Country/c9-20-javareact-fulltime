45124cd1ea2e231c975b644f7e66fd479a3ed693
Julio <alvarezjjoel6@gmail.com>
2023-02-27 15:56:58 -0300

    Add `CartMapper`

--- backend/src/main/java/ar/com/country/restaurant/web/mappers/CartMapper.java
+++ backend/src/main/java/ar/com/country/restaurant/web/mappers/CartMapper.java
@@ -1,2 +1,16 @@
-package ar.com.country.restaurant.web.mappers;public interface CartMapper {
+package ar.com.country.restaurant.web.mappers;
+
+import ar.com.country.restaurant.dao.entities.Cart;
+import ar.com.country.restaurant.web.dto.CartDTO;
+import ar.com.country.restaurant.web.dto.DishDTO;
+import ar.com.country.restaurant.web.dto.ItemCartDTO;
+import ar.com.country.restaurant.web.dto.UserDTO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = {DishDTO.class, ItemCartDTO.class, UserDTO.class})
+public interface CartMapper {
+
+    Cart toEntity(CartDTO cartDto);
+
+    CartDTO toDto(Cart cart);
 }
