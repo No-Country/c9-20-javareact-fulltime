b125b40bf5f532fc85079d8759941e68bee8cd52
Your Name <you@example.com>
2023-02-28 05:18:13 -0400

    all styles finish

--- frontend/src/pages/Admin/AdminCreate.jsx
+++ frontend/src/pages/Admin/AdminCreate.jsx
@@ -3,10 +3,11 @@ import { AdminCreateStyle } from "../../styled-components/Admin.styled";
 import DishesList from "./DishesList";
 import CategoryList from "./CategoryList";
 import CreateCategory from "./CreateCategory";
+import CreateDish from "./CreateDish";
 
 const AdminCreate = () => {
   //Este estado va a definir en un string cual es el componente a mostrar.
-  const [mostrar, setMostrar] = React.useState('Platos_Listado')
+  const [mostrar, setMostrar] = React.useState("Platos_Listado");
   return (
     <>
       <AdminCreateStyle>
@@ -23,26 +24,32 @@ const AdminCreate = () => {
         </nav>
         <div className="container">
           <div className="list">
-            <p className='secciones'>Platos</p >
-            <button onClick={()=> setMostrar('Platos_Listado')}>Listado</button>
-            <button onClick={()=> setMostrar('Platos_Listado')}>Agregar</button>
-            <p className='secciones'>Categoria</p >
-            <button onClick={()=> setMostrar('Categoria_Listado')}>Listado</button>
-            <button onClick={()=> setMostrar('Crear Categoria')}>Agregar</button>
-            <p className='secciones'>Ventas</p >
-            <button>Listado</button>
+            <p className="secciones">Platos</p>
+            <button
+              onClick={() => {
+                setMostrar("Platos_Listado");
+                style = { color: "red" };
+              }}
+            >
+              Listado
+            </button>
+            <button onClick={() => setMostrar("a")}>Agregar</button>
+            <p className="secciones">Categoria</p>
+            <button onClick={() => setMostrar("Categoria_Listado")}>
+              Listado
+            </button>
+            <button onClick={() => setMostrar("Crear Categoria")}>
+              Agregar
+            </button>
+            {/* <p className="secciones">Ventas</p>
+            <button>Listado</button> */}
           </div>
           <div className="component">
-
-             
-              {(mostrar == 'Platos_Listado') && <DishesList />}
-              {(mostrar == 'a') && 'Aqui va el componente'}
-              {(mostrar == 'Categoria_Listado') && <CategoryList />}
-              {(mostrar == 'Crear Categoria') && <CreateCategory />}
-              {(mostrar == 'c') && 'Aqui va el componente'}
-              
-            
-            
+            {mostrar == "Platos_Listado" && <DishesList />}
+            {mostrar == "a" && <CreateDish />}
+            {mostrar == "Categoria_Listado" && <CategoryList />}
+            {mostrar == "Crear Categoria" && <CreateCategory />}
+            {mostrar == "c" && "Aqui va el componente"}
           </div>
         </div>
       </AdminCreateStyle>
