5d031f226330e7d0915bcc53fdf157649b835b36
Manuel <nacermanuel@gmail.com>
2023-02-28 14:01:21 -0500

    Agregada componentes admin

---
+++ frontend/src/pages/Admin/AdminCreate.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { AdminCreateStyle } from "../../styled-components/Admin.styled";
+import DishesList from "./DishesList";
+import CategoryList from "./CategoryList";
+import CreateCategory from "./CreateCategory";
+import CreateDish from "./CreateDish";
+
+const AdminCreate = () => {
+  //Este estado va a definir en un string cual es el componente a mostrar.
+  const [mostrar, setMostrar] = React.useState("Platos_Listado");
+  return (
+    <>
+      <AdminCreateStyle>
+        <nav>
+          <img src="../../public/icons/logo.svg" alt="" />
+          <div className="administrator">
+            <object
+              width="17px"
+              data="../../public/icons/usersvg.svg"
+              type=""
+            ></object>
+            <a>Administrador</a>
+          </div>
+        </nav>
+        <div className="container">
+          <div className="list">
+            <p className="secciones">Platos</p>
+            <button
+              onClick={() => {
+                setMostrar("Platos_Listado");
+                style = { color: "red" };
+              }}
+            >
+              Listado
+            </button>
+            <button onClick={() => setMostrar("a")}>Agregar</button>
+            <p className="secciones">Categoria</p>
+            <button onClick={() => setMostrar("Categoria_Listado")}>
+              Listado
+            </button>
+            <button onClick={() => setMostrar("Crear Categoria")}>
+              Agregar
+            </button>
+            {/* <p className="secciones">Ventas</p>
+            <button>Listado</button> */}
+          </div>
+          <div className="component">
+            {mostrar == "Platos_Listado" && <DishesList />}
+            {mostrar == "a" && <CreateDish />}
+            {mostrar == "Categoria_Listado" && <CategoryList />}
+            {mostrar == "Crear Categoria" && <CreateCategory />}
+            {mostrar == "c" && "Aqui va el componente"}
+          </div>
+        </div>
+      </AdminCreateStyle>
+    </>
+  );
+};
+
+export default AdminCreate;
