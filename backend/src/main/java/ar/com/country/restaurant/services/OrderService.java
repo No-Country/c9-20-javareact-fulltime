ff8229cbf17bf85c2fcd6603abee7182b5a9422a
Julio <alvarezjjoel6@gmail.com>
2023-02-28 23:36:37 -0300

    Add `OrderService`

--- backend/src/main/java/ar/com/country/restaurant/services/OrderService.java
+++ backend/src/main/java/ar/com/country/restaurant/services/OrderService.java
@@ -1,2 +1,16 @@
-package ar.com.country.restaurant.services;public interface OrderService {
+package ar.com.country.restaurant.services;
+
+import ar.com.country.restaurant.dao.entities.Order;
+
+import java.util.List;
+
+public interface OrderService {
+
+    List<Order> getOrdersOfUser(Long userId);
+
+    Order getOrderOfUser(Long userId);
+
+    Order createOrder(Long userId, Long paymentMethodId);
+
+    void cancelOrder(Order order);
 }
