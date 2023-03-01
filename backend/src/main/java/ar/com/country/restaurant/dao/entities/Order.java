e17da768cf8ad7ad04188d4ba73540ed9bb50a3a
Julio <alvarezjjoel6@gmail.com>
2023-02-28 23:34:43 -0300

    Add methods to copy entities data

--- backend/src/main/java/ar/com/country/restaurant/dao/entities/Order.java
+++ backend/src/main/java/ar/com/country/restaurant/dao/entities/Order.java
@@ -16,21 +16,33 @@ import java.util.List;
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
     private Long id;
 
     @Column
     private Date createdAt;
 
-    @Column
-    private Double total;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private CardType cardType;
+
+    @Column
+    private String cardNumber;
+
     @ManyToOne
     @JoinColumn(name = "cart_id")
     private Cart cart;
+
+    @Column
+    private Double total;
 
     @OneToMany(
             mappedBy = "order",
@@ -40,8 +52,6 @@ public class Order {
     )
     private List<CartItem> items;
 
-    @OneToOne(cascade = CascadeType.ALL)
-    private PaymentMethod paymentMethod;
 
     @OneToOne(
             mappedBy = "order",
@@ -50,11 +60,39 @@ public class Order {
     )
     private Receipt receipt;
 
+
     public void generateReceipt() {
         this.receipt = Receipt.builder()
                 .order(this)
                 .build();
     }
 
+    public String endingCardNumbers(String number) {
+        return number.substring(number.length() - 4);
+    }
+
+    public void copyNameOfCartUser(Cart cart) {
+        this.username = cart.getUser().getName();
+        user.setName(this.username);
+    }
+
+    public void copyItemsFromCart(Cart cart) {
+        // copy items to a new list
+        List<CartItem> cartItems = cart.getItems();
+        // set the order of each item
+        cartItems.forEach(item -> item.setOrder(this));
+        // set the items of the order
+        this.items = cartItems;
+    }
+
+
+    public void copyTotalFromCart(Cart cart) {
+        this.total = cart.getSubTotal();
+    }
+
+    public void copyCardFromUser(PaymentMethod paymentMethod) {
+        this.cardType = paymentMethod.getType();
+        this.cardNumber = endingCardNumbers(paymentMethod.getNumber());
+    }
 }
 
