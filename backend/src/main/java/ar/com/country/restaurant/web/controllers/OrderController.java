78234c1c407d286518f0ef1dd5562065f943beb9
HikingCarrot7 <ricardoibarra2044@gmail.com>
2023-03-02 09:04:22 -0600

    Refactor order controller layer

--- backend/src/main/java/ar/com/country/restaurant/web/controllers/OrderController.java
+++ backend/src/main/java/ar/com/country/restaurant/web/controllers/OrderController.java
@@ -1,8 +1,10 @@
 package ar.com.country.restaurant.web.controllers;
 
-import ar.com.country.restaurant.dao.entities.Order;
+import ar.com.country.restaurant.dao.entities.order.Order;
 import ar.com.country.restaurant.security.SecurityUser;
 import ar.com.country.restaurant.services.OrderService;
+import ar.com.country.restaurant.web.dto.order.OrderDTO;
+import ar.com.country.restaurant.web.hateoas.assemblers.OrderModelAssembler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -11,10 +13,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 import static ar.com.country.restaurant.util.ApiDocsConstants.*;
@@ -29,52 +33,62 @@ import static ar.com.country.restaurant.util.ApiDocsConstants.*;
         @ApiResponse(ref = INTERNAL_SERVER_ERROR_RESPONSE_REF, responseCode = "500")
 })
 public class OrderController {
-
     private final OrderService orderService;
+    private final OrderModelAssembler orderModelAssembler;
 
-    @Operation(summary = "Returns the orders of the user with the given id")
+    @Operation(summary = "Returns the orders of the user")
     @ApiResponse(responseCode = "200", description = "OK", content = {
-        @Content(array = @ArraySchema(schema = @Schema(implementation = Order.class)))
-    })
-    @GetMapping("/{userId}")
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
-    public List<Order> getOrdersOfUser(@PathVariable Long userId) {
-        return orderService.getOrdersOfUser(userId);
-    }
-
-    @Operation(summary = "Returns the order of the logged user")
-    @ApiResponse(responseCode = "200", description = "OK", content = {
-        @Content(schema = @Schema(implementation = Order.class))
+            @Content(array = @ArraySchema(schema = @Schema(implementation = OrderDTO.class)))
     })
     @GetMapping
-    public Order getOrderOfLoggedUser(
-            @AuthenticationPrincipal SecurityUser loggedUser
+    public CollectionModel<OrderDTO> getOrdersOfLoggedUser(@AuthenticationPrincipal SecurityUser loggedUser) {
+        Long userId = loggedUser.getId();
+        List<Order> result = orderService.getOrdersOfUser(userId);
+        return orderModelAssembler.toCollectionModel(result);
+    }
+
+    @Operation(summary = "Returns the order with the given id of the logged user")
+    @ApiResponse(responseCode = "200", description = "OK", content = {
+            @Content(schema = @Schema(implementation = OrderDTO.class))
+    })
+    @GetMapping("/{orderId}")
+    public OrderDTO getOrderById(
+            @AuthenticationPrincipal SecurityUser loggedUser,
+            @PathVariable Long orderId
     ) {
         Long userId = loggedUser.getId();
-        return orderService.getOrderOfUser(userId); // return null (see OrderServiceImpl.java)
+        Order result = orderService.getOrderById(userId, orderId);
+        return orderModelAssembler.toModel(result);
     }
 
     @Operation(summary = "Creates an order for the logged user")
-    @ApiResponse(responseCode = "200", description = "OK", content = {
-        @Content(schema = @Schema(implementation = Order.class))
+    @ApiResponse(responseCode = "201", description = "OK", content = {
+            @Content(schema = @Schema(implementation = OrderDTO.class))
     })
     @PostMapping
-    public Order createOrder(
+    public ResponseEntity<OrderDTO> createOrder(
             @AuthenticationPrincipal SecurityUser loggedUser,
             @RequestParam Long paymentMethodId
     ) {
         Long userId = loggedUser.getId();
-        return orderService.createOrder(userId, paymentMethodId);
+        Order result = orderService.createOrder(userId, paymentMethodId);
+        return ResponseEntity
+                .created(URI.create("/api/orders/" + result.getId()))
+                .body(orderModelAssembler.toModel(result));
     }
 
     @Operation(summary = "Cancels the order of the logged user")
-    @ApiResponse(responseCode = "200", description = "OK")
-    @PostMapping("/cancel")
-    public void cancelOrder(
-            @AuthenticationPrincipal SecurityUser loggedUser
+    @ApiResponse(responseCode = "200", description = "OK", content = {
+            @Content(schema = @Schema(implementation = OrderDTO.class))
+    })
+    @PostMapping("/{orderId}/cancel")
+    public OrderDTO cancelOrder(
+            @AuthenticationPrincipal SecurityUser loggedUser,
+            @PathVariable Long orderId
     ) {
         Long userId = loggedUser.getId();
-        Order order = orderService.getOrderOfUser(userId);
-        orderService.cancelOrder(order);
+        Order order = orderService.cancelOrder(userId, orderId);
+        return orderModelAssembler.toModel(order);
     }
+
 }
