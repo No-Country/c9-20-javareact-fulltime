96862302009cdbd1de44e2529048ed4dda40709a
Gaston Gutierrez <gastongutierrez964@gmail.com>
2023-03-02 08:33:13 -0300

    Search Fix 1.2

--- frontend/src/pages/home/components/SearchLayer.jsx
+++ frontend/src/pages/home/components/SearchLayer.jsx
@@ -7,6 +7,7 @@ import {
 } from "../styled-components/layout.styled";
 import exit from "/icons/exit.svg";
 import img from "/img/fondoHome.jpg";
+
 const SearchLayer = ({ active, template, isSuccess, handleDesActive }) => {
 	return (
 		<SearchLayerStyled img={img} size='cover' active={active}>
@@ -15,23 +16,26 @@ const SearchLayer = ({ active, template, isSuccess, handleDesActive }) => {
 				<ScrollContainerStyled>
 					{isSuccess ? (
 						<div>
-							{template.map((item) => (
-								<Items
-									key={item.id}
-									category={item.category.name}
-									description={item.description}
-									id={item.id}
-									image={item.image.url}
-									people={item.people}
-									price={item.price}
-									title={item.name}
-									blockSize='192px'
-									inlineSize='300px'
-									insetInline="10%"
-									top='-15%'
-									hasPromotion={item.hasPromotion}
-								/>
-							))}
+							{template.length === 0 ? (<p>No hay publicaciones que coincidan con tu búsqueda.
+								Te invitamos a navegar por las categorías para encontrar el plato que buscas.</p>) :
+								(template.map((item) => (
+									<Items
+										key={item.id}
+										category={item.category.name}
+										description={item.description}
+										id={item.id}
+										image={item.image.url}
+										people={item.people}
+										price={item.price}
+										title={item.name}
+										blockSize='192px'
+										inlineSize='300px'
+										insetInline="10%"
+										top='-15%'
+										hasPromotion={item.hasPromotion}
+									/>
+								)))
+							}
 						</div>
 					) : (
 						<div>loading...</div>
