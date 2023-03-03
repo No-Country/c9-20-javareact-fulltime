035c5e52b004e43987aa47c2a6b2301c024d0be7
Your Name <you@example.com>
2023-03-03 01:16:58 -0400

    some styles changes

--- frontend/src/pages/view/viewoftheplate/hook/useComment.jsx
+++ frontend/src/pages/view/viewoftheplate/hook/useComment.jsx
@@ -3,33 +3,32 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useAddNewPostMutation } from "../../../../redux/query/FoodInfo.query";
 const useComment = () => {
-	const [comment, setComment] = useState("");
-	const { idFood } = useParams();
-	const token = useSelector((state) => state.auth.accessToken);
-	const [addNewPost, { status }] = useAddNewPostMutation();
-	const handleChange = (event) => {
-		setComment(event.target.value);
-	};
+  const [comment, setComment] = useState("");
+  const { idFood } = useParams();
+  const token = useSelector((state) => state.auth.accessToken);
+  const [addNewPost, { status }] = useAddNewPostMutation();
+  const handleChange = (event) => {
+    setComment(event.target.value);
+  };
 
-	const post = {
-		id: Number(idFood),
-		token,
-		content: JSON.stringify({ content: comment }),
-	};
+  const post = {
+    id: Number(idFood),
+    token,
+    content: JSON.stringify({ content: comment }),
+  };
 
-	const onSavePost = async () => {
-		if (post.content.comment !== "") {
-			await addNewPost(post, token).unwrap();
-			setComment("");
-		}
-	};
+  const onSavePost = async () => {
+    if (post.content.comment !== "") {
+      await addNewPost(post, token).unwrap();
+      setComment("");
+    }
+  };
 
-	return {
-		comment,
-		handleChange,
-		onSavePost,
-		handleChange,
-	};
+  return {
+    comment,
+    onSavePost,
+    handleChange,
+  };
 };
 
 export default useComment;
