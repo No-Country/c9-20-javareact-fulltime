35da134169fcf787d5e5b07434b233d4327e8b19
Your Name <you@example.com>
2023-05-31 20:54:52 -0400

    remove star again

--- frontend/src/components/Star.jsx
+++ frontend/src/components/Star.jsx
@@ -1,12 +1,16 @@
 import { StarStyled } from "../pages/view/categoria/styled-components/layout.styled";
 import star from "/assets/icons/startYellow.svg";
 const Star = () => {
-	return (
-		<StarStyled>
-			<img src={star} alt='star' />
-			<span>4.9</span>
-		</StarStyled>
-	);
+  // return (
+  // <StarStyled>
+  {
+    /* <img src={star} alt='star' /> */
+  }
+  {
+    /* <span>4.9</span> */
+  }
+  // </StarStyled>
+  // );
 };
 
 export default Star;
