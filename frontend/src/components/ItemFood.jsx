98e6840ac81cf3b143c5e633e8170965c01febbe
Your Name <you@example.com>
2023-05-31 20:14:44 -0400

    star remove

--- frontend/src/components/ItemFood.jsx
+++ frontend/src/components/ItemFood.jsx
@@ -3,38 +3,38 @@ import { ItemFoodStyled } from "../pages/view/categoria/styled-components/layout
 import Star from "./Star";
 import cart from "/assets/icons/cart.svg";
 const ItemFood = ({
-	description,
-	price,
-	nameFood,
-	portion,
-	id,
-	img,
-	category,
+  description,
+  price,
+  nameFood,
+  portion,
+  id,
+  img,
+  category,
 }) => {
-	const { namePath } = useParams();
-	return (
-		<>
-			{!namePath ? (
-				<ItemFoodStyled>
-					<header>
-						<img src={img} alt={nameFood} loading='lazy' />
-					</header>
-					<Star />
-					<h2>{nameFood}</h2>
-					<b>{description}</b>
-					<p>{portion} porciones</p>
-					<strong>${price}</strong>
+  const { namePath } = useParams();
+  return (
+    <>
+      {!namePath ? (
+        <ItemFoodStyled>
+          <header>
+            <img src={img} alt={nameFood} loading="lazy" />
+          </header>
+          {/* <Star /> */}
+          <h2>{nameFood}</h2>
+          <b>{description}</b>
+          <p>{portion} porciones</p>
+          <strong>${price}</strong>
 
-					<Link to={`/categoria/${category}/${id}`}>
-						<strong>Agregar al Carrito</strong>
-						<img src={cart} alt="image-cart" />
-					</Link>
-				</ItemFoodStyled>
-			) : (
-				<Outlet />
-			)}
-		</>
-	);
+          <Link to={`/categoria/${category}/${id}`}>
+            <strong>Agregar al Carrito</strong>
+            <img src={cart} alt="image-cart" />
+          </Link>
+        </ItemFoodStyled>
+      ) : (
+        <Outlet />
+      )}
+    </>
+  );
 };
 
 export default ItemFood;
